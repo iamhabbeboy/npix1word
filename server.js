65eaa80bf9e78e8fc73884ba1fbf397bcfda77c2
iamhabbeboy <iamhabbeboy@gmail.com>
2020-05-02 13:45:51 +0100

    re-block code

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-// server.js
 var express = require("express");
 var path = require("path");
 var serveStatic = require("serve-static");
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
-var hostname = "127.0.0.1";
-
-app.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`); //eslint-disable-line
-});
+app.listen(port);
+console.log("server started " + port); //eslint-disable-line
