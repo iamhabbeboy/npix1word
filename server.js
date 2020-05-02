fba46ae14bc4aa165330b43cd6d98c46eaead8cf
iamhabbeboy <iamhabbeboy@gmail.com>
2020-05-02 14:18:31 +0100

    server code re-write rule

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-var express = require("express");
-var path = require("path");
-var serveStatic = require("serve-static");
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log("server started " + port); //eslint-disable-line
+const express = require("express");
+const history = require("connect-history-api-fallback");
+const app = express();
+
+const staticFileMiddleware = express.static(__dirname + "/dist"); //app.use(serveStatic(__dirname + "/dist"));
+app.use(staticFileMiddleware);
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true
+  })
+);
+app.use(staticFileMiddleware);
+
+const port = 5555;
+app.listen(port, () => {
+console.log(`App listening on port ${port}!`); //eslint-disable-line
+});
